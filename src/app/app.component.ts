07276e951912da5fcf9f110dcea111fdc9ecd58e
Le Nguyen <dlnguyen@gmail.com>
2025-04-03 10:36:02 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {StudentListComponent} from './student-list/student-list.component';
+import {StudentAddComponent} from './student-add/student-add.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StudentListComponent, StudentAddComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
